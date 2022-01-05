c40a6f9689b2e32135efa2aab77eecba9a6a47ff
eneotu joe <eneotujoe@gmail.com>
2022-01-05 15:58:38 +0100

    update sales page

--- frontend/pages/sales.vue
+++ frontend/pages/sales.vue
@@ -1,7 +1,94 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12">
-      <h1>sales</h1>
-    </v-col>
-  </v-row>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Product',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Customer ID', value: 'cutomer' },
+          { text: 'Quantity', value: 'quantity' },
+          { text: 'Cost ( Naira )', value: 'cost' },
+          { text: 'Date', value: 'date' },
+        ],
+        desserts: [
+          {
+            name: 'Yogurt',
+            cutomer: 159,
+            quantity: 60,
+            cost: 3000,
+            date: '01-01-2022',
+          },
+          {
+            name: 'Ice cream',
+            cutomer: 237,
+            quantity: 90,
+            cost: 9200,
+            date: '01-01-2022',
+          },
+          {
+            name: 'Cupcake',
+            cutomer: 305,
+            quantity: 33,
+            cost: 1500,
+            date: '01-01-2022',
+          },
+          {
+            name: 'Gingerbread',
+            cutomer: 356,
+            quantity: 116,
+            cost: 6500,
+            date: '01-01-2022',
+          },
+          {
+            name: 'Jelly bean',
+            cutomer: 375,
+            quantity: 28,
+            cost: 400,
+            date: '01-01-2022',
+          },
+          {
+            name: 'Lollipop',
+            cutomer: 392,
+            quantity: 20,
+            cost: 2000,
+            date: '01-01-2022',
+          },
+          {
+            name: 'Honeycomb',
+            cutomer: 408,
+            quantity: 32,
+            cost: 3200,
+            date: '01-01-2022',
+          },
+          {
+            name: 'Donut',
+            cutomer: 452,
+            quantity: 25,
+            cost: 625,
+            date: '01-01-2022',
+          },
+          {
+            name: 'KitKat',
+            cutomer: 518,
+            quantity: 26,
+            cost: 2600,
+            date: '01-01-2022',
+          },
+        ],
+      }
+    },
+  }
+</script>
